55db17c0fe50b57c5fcb8bb8f7bd8b67045f679f
Alexey <alexey.anasov@yandex.by>
2024-09-02 20:51:09 +0300

    навигация

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
 $(document).ready(function () {
-  /* Call plugins hrere ...*/
+  // Скрипт для показа и скрытия выпадающего меню на смартфонах
+  // Создаем переменые для кнопки и для меню
+  var pull = $("#navigation-toggle");
+  var menu = $(".navigation ul");
+
+  // Описываем событие при нажатии на кнопку
+  $(pull).on("click", function (e) {
+    // Отменяем стандартное поведение ссылки в браузере
+    e.preventDefault();
+
+    // Открываем/Скрываем меню
+    menu.slideToggle();
+
+    // Добавляем модификатор --active
+    $(this).toggleClass("navigation__toggle-button--active");
+  });
+
+  // При изменении размера окна, в большую сторону, если меню было с крыто, показываем его.
+  $(window).resize(function () {
+    var w = $(window).width();
+    if (w > 992) {
+      menu.removeAttr("style");
+    }
+  });
 });
