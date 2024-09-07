6f36edbca45a445a2e9eaa527f20e2cefd4d6d6a
Alexey <alexey.anasov@yandex.by>
2024-09-07 20:30:52 +0300

    Футер

--- js/main.js
+++ js/main.js
@@ -1,40 +1,34 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  // Скрипт для показа и скрытия выпадающего меню на смартфонах
+  // Создаем переменые для кнопки и для меню
+  var pull = $("#navigation-toggle");
+  var menu = $(".navigation ul");
 
-	// Скрипт для показа и скрытия выпадающего меню на смартфонах
-	// Создаем переменые для кнопки и для меню
-	var pull = $('#navigation-toggle');
-	var menu = $('.navigation ul');
+  // Описываем событие при нажатии на кнопку
+  $(pull).on("click", function (e) {
+    // Отменяем стандартное поведение ссылки в браузере
+    e.preventDefault();
 
-	// Описываем событие при нажатии на кнопку
-	$(pull).on("click", function(e){
+    // Открываем/Скрываем меню
+    menu.slideToggle();
 
-	    // Отменяем стандартное поведение ссылки в браузере
-	    e.preventDefault();
+    // Добавляем модификатор --active
+    $(this).toggleClass("navigation__toggle-button--active");
+  });
 
-	    // Открываем/Скрываем меню
-	    menu.slideToggle();
+  // При изменении размера окна, в большую сторону, если меню было с крыто, показываем его.
+  $(window).resize(function () {
+    var w = $(window).width();
+    if (w > 992) {
+      menu.removeAttr("style");
+    }
+  });
 
-	    // Добавляем модификатор --active
-	    $(this).toggleClass('navigation__toggle-button--active');
-	    
-	});
-
-	// При изменении размера окна, в большую сторону, если меню было с крыто, показываем его.
-	$(window).resize(function(){
-	    var w = $(window).width();
-	    if(w > 992) {
-	        menu.removeAttr('style');
-	    }
-	});
-
-
-	// Вызов слайдера owl-сarousel
-	$("#top-slider").owlCarousel({
-		singleItem: true, //отображать один слайд
-		navigation: true, //отображает стрелки влево, вправо
-		theme: "top-slider-theme", //наша новая тема
-		navigationText: ["", ""] //убирает текст с кнопок (лево, право)
-	});
-	
+  // Вызов слайдера owl-сarousel
+  $("#top-slider").owlCarousel({
+    singleItem: true, //отображать один слайд
+    navigation: true, //отображает стрелки влево, вправо
+    theme: "top-slider-theme", //наша новая тема
+    navigationText: ["", ""], //убирает текст с кнопок (лево, право)
+  });
 });
-
