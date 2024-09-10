77e28cf270c04e0aefce91a7f7e62a89b8d24e3f
Alexey <alexey.anasov@yandex.by>
2024-09-10 20:17:02 +0300

    final проекта

--- js/main.js
+++ js/main.js
@@ -24,11 +24,26 @@ $(document).ready(function () {
     }
   });
 
+  //скрытие меню по клику
+  var w = $(window).width(); //ширина
+  if (w < 992) {
+    $("nav.navigation a").on("click", function () {
+      menu.slideToggle();
+    });
+  }
+
   // Вызов слайдера owl-сarousel
   $("#top-slider").owlCarousel({
     singleItem: true, //отображать один слайд
     navigation: true, //отображает стрелки влево, вправо
     theme: "top-slider-theme", //наша новая тема
     navigationText: ["", ""], //убирает текст с кнопок (лево, право)
+  });
+
+  //slide2id - плавная прокрутка по ссылкам
+  $(
+    "nav a,a[href='#top'],a[rel='m_PageScroll2id'],a.PageScroll2id"
+  ).mPageScroll2id({
+    highlightSelector: "nav a",
   });
 });
